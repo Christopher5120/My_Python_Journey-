d70bb5076908310ecc9471cfd1cf0d27574edd74
Chris DePythonista <65058251+Christopher5120@users.noreply.github.com>
2021-12-07 08:39:20 +0530

    Create Stripping_spaces.py

---
+++ Stripping_spaces.py
@@ -0,0 +1,6 @@
+"""Stripping/removing spaces from the given variables"""
+string1, string2, string3 = " Filet Mignon", "Brisket", "Cheeseburger "
+
+print(string1.lstrip())
+print(string2.rstrip())
+print(string3.rstrip())
