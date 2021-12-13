b8649c1d52cb4cc537e96aff48498f64d230e327
Chris DePythonista <65058251+Christopher5120@users.noreply.github.com>
2021-12-13 21:56:41 +0530

    Create Multiplying_different_integers.py

---
+++ Multiplying_different_integers.py
@@ -0,0 +1,6 @@
+""" Create a string containing an integer, then convert that string into an actual integer object using int(). Test that your new object is a number by multiplying it by another number and displaying the result. """
+
+y = 5
+z = int(y)
+d = 8 * z
+print(d)
