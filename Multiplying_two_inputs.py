1ee31b83bb2cb6fe1a84e79b15dc8e7835665ed0
Chris DePythonista <65058251+Christopher5120@users.noreply.github.com>
2021-12-13 22:01:33 +0530

    Create Multiplying_two_inputs.py

---
+++ Multiplying_two_inputs.py
@@ -0,0 +1,7 @@
+"""Write a program that uses input() twice to get two numbers from the user, multiplies the numbers together, and displays the result. If the user enters 2 and 4, then your program should print the following text:
+The product of 2 and 4 is 8.0."""
+
+number_1 = int(input("Enter a number: "))
+number_2 = int(input("Enter a number: "))
+h = float(number_1 * number_2
+print(f" The product of {number_1} and {number_2} is {h}" )
