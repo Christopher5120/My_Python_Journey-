9a6a93e737396a14fe10d78634997693c806a5b3
Chris DePythonista <65058251+Christopher5120@users.noreply.github.com>
2021-12-13 21:58:39 +0530

    Create Rounding_an_input_2_dps.py

---
+++ Rounding_an_input_2_dps.py
@@ -0,0 +1,7 @@
+
+""" Write a program that asks the user to input a number and then displays that number rounded to two decimal places. When run, your program should look like this:
+  Enter a number: 5.432
+5.432 rounded to 2 decimal places is 5.43"""
+
+k = float(input("Enter a number: "))
+print(round(k,2))
